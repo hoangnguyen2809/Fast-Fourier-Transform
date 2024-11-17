eb938a5f166d56a585b625e293bb9d860b291bfe
anhhoang289 <viethoangthp289@gmail.com>
2024-11-16 18:10:28 -0800

    update fft

--- fast_fourier_transform_serial.cpp
+++ fast_fourier_transform_serial.cpp
@@ -19,51 +19,64 @@ int reverseBits(int x, int numBits) {
     return reversed;
 }
 
-void fft(std::vector<Complex>& X) {
-    int n = X.size();
+void fft(std::vector<Complex>& X, std::vector<Complex>& Y, int n) {
+    timer serial_timer;
+    double time_taken = 0.0;
+    serial_timer.start();
+    
+    //*------------------------------------------------------------------------
     int r = log2(n);
 
-    std::vector<Complex> Y(n);
+    std::vector<Complex> R(n);
+    std::vector<Complex> S(n);
+
     for (int i = 0; i < n; ++i) {
         int revIndex = reverseBits(i, r);
         Y[revIndex] = X[i];
     }
-
+    
     for (int m = 1; m <= r; ++m) {
         int step = 1 << m; // 2^m
-        Complex w_m = exp(Complex(0, -2.0 * M_PI / step)); 
+        Complex w_m = exp(Complex(0, -2.0 * M_PI / step)); // twiddle factor (root of unity)
 
         for (int k = 0; k < n; k += step) {
-            Complex w = 1; 
+            Complex w = 1;
             for (int j = 0; j < step / 2; ++j) {
                 Complex t = w * Y[k + j + step / 2];
                 Complex u = Y[k + j];
                 Y[k + j] = u + t;
                 Y[k + j + step / 2] = u - t;
-                
-                w *= w_m;
+                                
+                w *= w_m; // update twiddle factor
             }
         }
     }
 
-    // Copy the result back to X
-    for (int i = 0; i < n; ++i) {
-        X[i] = Y[i];
+    
+    //------------------------------------------------------------------------* 
+    time_taken = serial_timer.stop();
+    
+    // Output the results
+    std::cout << "FFT result:" << std::endl;
+    for (const auto& value : Y) {
+        std::cout << value << std::endl;
     }
+    
+    std::cout << "Time taken (in seconds) : " << std::setprecision(TIME_PRECISION) << time_taken << "\n";
 }
 
 int main() {
+    // Initialize command line arguments
+    cxxopts::Options options("Fast Fourier Transform Computation",
+                            "Computes the Discrete Fourier Transform (DFT) of a sequence");
+    
+
     // Sample input (size must be a power of 2)
     std::vector<Complex> X = { 1, 2, 3, 4 };
-
-    // Perform FFT
-    fft(X);
-
-    // Output the results
-    std::cout << "FFT result:" << std::endl;
-    for (const auto& value : X) {
-        std::cout << value << std::endl;
-    }
-
+    int n = X.size();
+    std::vector<Complex> Y(n);
+    
+    fft(X, Y, n);
+    
     return 0;
 }
